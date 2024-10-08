3b80700e0d7b16a150aeda0844a79f812b08cfe1
IreneRomero <irene_romero_95@hotmail.com>
2024-10-08 21:13:09 +0200

    correcion header

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 import './style.css'
 import { createButton } from './src/components/button/button'
-import { createApi } from "unsplash-js"
+import { createApi } from 'unsplash-js'
 import { cardTemplate } from './src/components/card/card'
 
-
 const unsplash = createApi({
-  accessKey: import.meta.env.VITE_ACCESS_KEY,
+  accessKey: import.meta.env.VITE_ACCESS_KEY
 })
 
 const searchPhotos = async (keyword) => {
   const images = await unsplash.search.getPhotos({
     query: keyword,
     page: 1,
-    perPage: 30,
+    perPage: 30
   })
   return images
 }
 
-
-// HEADER 
+// HEADER
 const headerTemplate = () => {
   return `
   <h1>P</h1>
+  <div id=busqueda>
   <input type="text" placeholder="Search" id="searchinput"/>
-  ${createButton({id:"searchbtn", img:"/assets/search.png", alt:"Search icon"})}
-  ${createButton({id:"darkmodebtn", img:"/assets/darkmode.png", alt:"Dark mode icon"})}
+  ${createButton({
+    id: 'searchbtn',
+    img: '/assets/search.png',
+    alt: 'Search icon'
+  })} </div>
+  ${createButton({
+    id: 'darkmodebtn',
+    img: '/assets/darkmode.png',
+    alt: 'Dark mode icon'
+  })}
   <img
     src="/assets/profilepic.png"
     alt="Profile image"
@@ -34,27 +41,27 @@ const headerTemplate = () => {
 }
 
 const themeSwitch = () => {
-  document.body.classList.toggle("dark")
+  document.body.classList.toggle('dark')
 }
 
 const listeners = () => {
-  const darkmodebtn = document.querySelector("#darkmodebtn")
-  darkmodebtn.addEventListener("click", () => {
+  const darkmodebtn = document.querySelector('#darkmodebtn')
+  darkmodebtn.addEventListener('click', () => {
     themeSwitch()
-    const theme = document.body.classList.contains("dark")
+    const theme = document.body.classList.contains('dark')
   })
 }
 
 const printHeaderTemplate = () => {
-  document.querySelector("header").innerHTML = headerTemplate()
+  document.querySelector('header').innerHTML = headerTemplate()
   listeners()
 }
 
-printHeaderTemplate();
+printHeaderTemplate()
 
-const reload = document.querySelector("h1");
-reload.addEventListener("click", () => {
-  location.reload();
+const reload = document.querySelector('h1')
+reload.addEventListener('click', () => {
+  location.reload()
 })
 
 //GALLERY
@@ -67,47 +74,43 @@ const galleryTemplate = () => {
 }
 
 const printItems = (items) => {
-  const gallery = document.querySelector(".gallery")
-  gallery.innerHTML = ""
+  const gallery = document.querySelector('.gallery')
+  gallery.innerHTML = ''
   for (const item of items) {
     gallery.innerHTML += cardTemplate(item)
   }
 }
 
 const galleryListeners = async () => {
-  const input = document.querySelector("#searchinput")
-  const searchbtn = document.querySelector("#searchbtn")
-  searchbtn.addEventListener("click", async () => {
+  const input = document.querySelector('#searchinput')
+  const searchbtn = document.querySelector('#searchbtn')
+  searchbtn.addEventListener('click', async () => {
     const images = await searchPhotos(input.value)
-    if (images.response.total != 0){
-    printItems(images.response.results)
-    }
-    else {
-      const images = await searchPhotos("gatos");
+    if (images.response.total != 0) {
       printItems(images.response.results)
-      const sugestion = document.createElement("h2")
-      sugestion.innerText = "Prueba otra frase o palabra"
-      const gallery = document.querySelector(".gallery")
-      const main = document.querySelector("main");
-      main.insertBefore(sugestion, gallery);
+    } else {
+      const images = await searchPhotos('gatos')
+      printItems(images.response.results)
+      const sugestion = document.createElement('h2')
+      sugestion.innerText = 'Prueba otra frase o palabra'
+      const gallery = document.querySelector('.gallery')
+      const main = document.querySelector('main')
+      main.insertBefore(sugestion, gallery)
     }
-    input.value = ""
+    input.value = ''
   })
 }
 
 export const printTemplate = async () => {
-  document.querySelector("main").innerHTML = galleryTemplate()
+  document.querySelector('main').innerHTML = galleryTemplate()
   galleryListeners()
-  const images = await searchPhotos("gatos");
+  const images = await searchPhotos('gatos')
   printItems(images.response.results)
 }
 
 printTemplate()
 
-
-
 // FOOTER
 
 import { printFooterTemplate } from './src/components/FOOTER/footer'
-printFooterTemplate();
-
+printFooterTemplate()
