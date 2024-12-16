95812ae2b8d74fd9f14e81e70e9c85c57a096f43
IreneRomero <irene_romero_95@hotmail.com>
2024-12-16 18:21:25 +0100

    mensaje se elimina

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-import './style.css'
-import { createButton } from './src/components/button/button'
-import { createApi } from 'unsplash-js'
-import { cardTemplate } from './src/components/card/card'
+import './style.css';
+import { createButton } from './src/components/button/button';
+import { createApi } from 'unsplash-js';
+import { cardTemplate } from './src/components/card/card';
 
 const unsplash = createApi({
   accessKey: import.meta.env.VITE_ACCESS_KEY
-})
+});
 
 const searchPhotos = async (keyword) => {
   const images = await unsplash.search.getPhotos({
     query: keyword,
     page: 1,
     perPage: 30
-  })
-  return images
-}
+  });
+  return images;
+};
 
 // HEADER
 const headerTemplate = () => {
@@ -37,32 +37,32 @@ const headerTemplate = () => {
     alt="Profile image"
     class="profileimg"
   /> 
-    `
-}
+    `;
+};
 
 const themeSwitch = () => {
-  document.body.classList.toggle('dark')
-}
+  document.body.classList.toggle('dark');
+};
 
 const listeners = () => {
-  const darkmodebtn = document.querySelector('#darkmodebtn')
+  const darkmodebtn = document.querySelector('#darkmodebtn');
   darkmodebtn.addEventListener('click', () => {
-    themeSwitch()
-    const theme = document.body.classList.contains('dark')
-  })
-}
+    themeSwitch();
+    const theme = document.body.classList.contains('dark');
+  });
+};
 
 const printHeaderTemplate = () => {
-  document.querySelector('header').innerHTML = headerTemplate()
-  listeners()
-}
+  document.querySelector('header').innerHTML = headerTemplate();
+  listeners();
+};
 
-printHeaderTemplate()
+printHeaderTemplate();
 
-const reload = document.querySelector('h1')
+const reload = document.querySelector('h1');
 reload.addEventListener('click', () => {
-  location.reload()
-})
+  location.reload();
+});
 
 //GALLERY
 
@@ -70,47 +70,52 @@ const galleryTemplate = () => {
   return `
     <ul class="gallery">
     </ul>
-    `
-}
+    `;
+};
 
 const printItems = (items) => {
-  const gallery = document.querySelector('.gallery')
-  gallery.innerHTML = ''
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
   for (const item of items) {
-    gallery.innerHTML += cardTemplate(item)
+    gallery.innerHTML += cardTemplate(item);
   }
-}
+};
 
 const galleryListeners = async () => {
-  const input = document.querySelector('#searchinput')
-  const searchbtn = document.querySelector('#searchbtn')
+  const input = document.querySelector('#searchinput');
+  const searchbtn = document.querySelector('#searchbtn');
   searchbtn.addEventListener('click', async () => {
-    const images = await searchPhotos(input.value)
-    if (images.response.total != 0) {
-      printItems(images.response.results)
-    } else {
-      const images = await searchPhotos('gatos')
-      printItems(images.response.results)
-      const sugestion = document.createElement('h2')
-      sugestion.innerText = 'Prueba otra frase o palabra'
-      const gallery = document.querySelector('.gallery')
-      const main = document.querySelector('main')
-      main.insertBefore(sugestion, gallery)
+    const mensaje = document.querySelector('.noexiste');
+    if (mensaje) {
+      mensaje.remove();
     }
-    input.value = ''
-  })
-}
+    const images = await searchPhotos(input.value);
+    if (images.response.total != 0) {
+      printItems(images.response.results);
+    } else {
+      const images = await searchPhotos('gatos');
+      printItems(images.response.results);
+      const sugestion = document.createElement('h2');
+      sugestion.className = 'noexiste';
+      sugestion.innerText = 'Prueba otra frase o palabra';
+      const gallery = document.querySelector('.gallery');
+      const main = document.querySelector('main');
+      main.insertBefore(sugestion, gallery);
+    }
+    input.value = '';
+  });
+};
 
 export const printTemplate = async () => {
-  document.querySelector('main').innerHTML = galleryTemplate()
-  galleryListeners()
-  const images = await searchPhotos('gatos')
-  printItems(images.response.results)
-}
+  document.querySelector('main').innerHTML = galleryTemplate();
+  galleryListeners();
+  const images = await searchPhotos('gatos');
+  printItems(images.response.results);
+};
 
-printTemplate()
+printTemplate();
 
 // FOOTER
 
-import { printFooterTemplate } from './src/components/FOOTER/footer'
-printFooterTemplate()
+import { printFooterTemplate } from './src/components/FOOTER/footer';
+printFooterTemplate();
